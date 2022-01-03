4b3d25d134ff74ae6d4e72ef9112a440b98a9aa8
rhodowen <41326230+rhod-dev@users.noreply.github.com>
2022-01-03 21:26:42 +0000

    Initial commit

---
+++ main.jl
@@ -0,0 +1,7 @@
+# widen the output after running!
+for i in 0:255
+    c= i== 0 ? "NUL" : i== 7 ? "BEL" : i== 8 ? "BKS" : i== 9 ? "TAB" :
+       i==10 ? "LF " : i==13 ? "CR " : i==27 ? "ESC" : i==155 ? "CSI" : "|$(Char(i))|"
+    print("$(lpad(i,3)) $(string(i,base=16,pad=2)) $c")
+    (i&7)==7 ? println() : print("  ")
+end
